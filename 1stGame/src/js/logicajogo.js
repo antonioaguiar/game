677d3ef888a9bd78fbde0b23f96d9ec912b96666
Antonio Aguiar <antonioaguiar@gmail.com>
2020-02-12 13:49:36 -0400

    upd

--- 1stGame/src/js/logicajogo.js
+++ 1stGame/src/js/logicajogo.js
@@ -4,12 +4,62 @@
 */
 function createGame() {
 
+
+    //criar jogadores e frutas
+    const state = {
+        players: {},
+        fruits: {
+            //'fruit1': { x: 4, y: 1 }
+        }
+    }
+
+
+    function addPlayer(command) {
+        const playerId = command.playerId
+        const playerX = command.playerX;
+        const playerY = command.playerY;
+        state.players[playerId] = {
+            x: playerX,
+            y: playerY
+        };
+    }
+
+    function removePlayer(command) {
+        const playerId = command.playerId;
+        delete state.players[playerId];
+    }
+
     function movePlayer(command) {
         console.log(`Moving ${command.playerId} with ${command.keyPressed}`);
 
+        const acceptedMovies = {
+            ArrowUp(player) {
+                console.log('Moving player UP');
+                player.y =  player.y - 1 >= 0 ? player.y - 1 : 0;
+            },
+            ArrowDown(player) {
+                console.log('Moving player Down');
+                player.y = player.y + 1 < screen.width ? player.y + 1 : screen.width - 1 ;
+            },
+            ArrowLeft(player) {
+                console.log('Moving player Left');
+                player.x = player.x - 1 >= 0 ? player.x - 1 : 0;
+            },
+            ArrowRight(player) {
+                console.log('Moving player Right');
+                player.x =  player.x + 1 < screen.height  ? player.x + 1: screen.height - 1;
+            }
+        }
+
         const player = state.players[currentPlayer];
         const keyPressed = command.keyPressed;
+        const moveFunction = acceptedMovies[keyPressed];
 
+        if (moveFunction)
+            moveFunction(player);
+
+        return;
+        /*
         if (keyPressed === 'ArrowUp' && player.y - 1 >= 0) {
             player.y = player.y - 1;
             return
@@ -26,9 +76,12 @@ function createGame() {
             player.x = player.x + 1;
             return
         }
+        */
     }
 
     return {
+        addPlayer,
+        removePlayer,
         movePlayer,
         state
     }
